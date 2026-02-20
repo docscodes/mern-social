5148bab0a2911c683bef7646298d33b7a28f506c
docscodes <docscodes@gmail.com>
2026-02-20 02:10:02 -0500

    update routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <Routes>
         <Route
           path="/"
-          element={<Home />}
+          element={user ? <Home /> : <Register />}
         ></Route>
         <Route
           path="/login"
-          element={<Login />}
+          element={user ? <Navigate to="/" /> : <Login />}
         ></Route>
         <Route
           path="/register"
-          element={<Register />}
+          element={user ? <Navigate to="/" /> : <Register />}
         ></Route>
         <Route
           path="/profile/:username"
