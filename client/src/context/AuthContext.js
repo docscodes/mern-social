fe170dbdd3b71db214c89570e84aeb52468b2cb4
docscodes <docscodes@gmail.com>
2026-02-23 18:29:55 -0500

    use auth context on profile page

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,8 +1,24 @@
 import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
+// const INITIAL_STATE = {
+//   user: JSON.parse(localStorage.getItem("user")) || null,
+//   isFetching: false,
+//   error: false,
+// };
+
 const INITIAL_STATE = {
-  user: JSON.parse(localStorage.getItem("user")) || null,
+  user: {
+    _id: "697b17e59805b7aeb42cb3d9",
+    username: "jane",
+    email: "jane@gmail.com",
+    password: "123456",
+    profilePicture: "person/noAvatar.png",
+    coverPicture: "person/noCover.png",
+    isAdmin: false,
+    followers: [],
+    followings: [],
+  },
   isFetching: false,
   error: false,
 };
