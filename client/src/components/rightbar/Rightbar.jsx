d290acbfec6b6af104cc1c6d9d06ea63ecfbc1c9
docscodes <docscodes@gmail.com>
2026-02-25 00:28:37 -0500

    dispatch follow actions to update auth context

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -11,12 +11,8 @@ export default function Rightbar({ user }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [friends, setFriends] = useState([]);
 
-  const { user: currentUser } = useContext(AuthContext);
-  const [followed, setFollowed] = useState(false);
-
-  useEffect(() => {
-    setFollowed(currentUser.followings.includes(user?._id));
-  }, [currentUser, user._id]);
+  const { user: currentUser, dispatch } = useContext(AuthContext);
+  const [followed, setFollowed] = useState(currentUser.followings.includes(user?._id));
 
   useEffect(() => {
     const getFriends = async () => {
@@ -39,10 +35,14 @@ export default function Rightbar({ user }) {
         await axios.put(`/users/${user._id}/unfollow`, {
           userId: currentUser._id,
         });
+
+        dispatch({ type: "UNFOLLOW", payload: user._id });
       } else {
         await axios.put(`/users/${user._id}/follow`, {
           userId: currentUser._id,
         });
+
+        dispatch({ type: "FOLLOW", payload: user._id });
       }
 
       setFollowed(!followed);
